5b53c90f0b5e90d01c6c5b5aa64535e5ec26ba98
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-04 20:21:32 +0530

    ADDED: Custom exception thrown for invalid details entry

--- main/java/com/bridgelabz/UserRegistrationValidation.java
+++ main/java/com/bridgelabz/UserRegistrationValidation.java
@@ -3,28 +3,43 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserRegistrationValidation {
-    public boolean validateFirstName(String firstName) {
-        return Pattern.matches("[A-Z][a-z]{3,15}", firstName);
+    public boolean validateFirstName(String firstName) throws UserRegistrationValidationException {
+        if (Pattern.matches("[A-Z][a-z]{3,15}", firstName)) {
+            return true;
+        } else {
+            throw new UserRegistrationValidationException("Entered invalid first name");
+        }
     }
 
-    public boolean validateLastName(String lastName) {
-        return Pattern.matches("[A-Z][a-z]{3,15}", lastName);
+    public boolean validateLastName(String lastName) throws UserRegistrationValidationException {
+        if (Pattern.matches("[A-Z][a-z]{3,15}", lastName)) {
+            return true;
+        } else {
+            throw new UserRegistrationValidationException("Entered invalid last name");
+        }
     }
 
-    public boolean validateEmailId(String emailId) {
-        return Pattern.matches("[a-z][a-z A-Z 0-9]+[@][a-z]+[.][a-z]{2,5}", emailId);
+    public boolean validateEmailId(String emailId) throws UserRegistrationValidationException {
+        if (Pattern.matches("[a-z][a-z A-Z 0-9]+[@][a-z]+[.][a-z]{2,5}", emailId)) {
+            return true;
+        } else {
+            throw new UserRegistrationValidationException("Entered invalid email id");
+        }
     }
 
-    public boolean validateMobileNumber(String mobileNumber) {
-        return Pattern.matches("[+]91 [6789][0-9]{9}", mobileNumber);
+    public boolean validateMobileNumber(String mobileNumber) throws UserRegistrationValidationException {
+        if (Pattern.matches("[+]91 [6789][0-9]{9}", mobileNumber)) {
+            return true;
+        } else {
+            throw new UserRegistrationValidationException("Entered invalid mobile number");
+        }
     }
 
-    public boolean validatePassword(String password) {
-        return Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z 0-9 $&+,:;=?@#|'<>.-^*()%!]{8,}", password);
+    public boolean validatePassword(String password) throws UserRegistrationValidationException {
+        if (Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z 0-9 $&+,:;=?@#|'<>.-^*()%!]{8,}", password)) {
+            return true;
+        } else {
+            throw new UserRegistrationValidationException("Entered invalid password");
+        }
     }
-
-    public boolean validateMultipleEmailId(String multipleEmailId) {
-        return Pattern.matches("[a-z][a-z A-Z 0-9 .+-]+[@][a-z 0-9]+[.][a-z]{2,5}", multipleEmailId);
-    }
-
 }
