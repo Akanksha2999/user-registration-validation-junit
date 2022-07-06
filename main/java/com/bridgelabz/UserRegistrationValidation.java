5bf88885fae99d778217118ca50fc48ab17d47aa
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-07-06 13:55:56 +0530

    ADDED: Custom exception thrown for invalid details entry

--- main/java/com/bridgelabz/UserRegistrationValidation.java
+++ main/java/com/bridgelabz/UserRegistrationValidation.java
@@ -3,6 +3,8 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
 public class UserRegistrationValidation {
+
+    //Regex pattern for first name
     public boolean validateFirstName(String firstName) throws UserRegistrationValidationException {
         if (Pattern.matches("[A-Z][a-z]{3,15}", firstName)) {
             return true;
@@ -11,6 +13,7 @@ public class UserRegistrationValidation {
         }
     }
 
+    //regex pattern for last name
     public boolean validateLastName(String lastName) throws UserRegistrationValidationException {
         if (Pattern.matches("[A-Z][a-z]{3,15}", lastName)) {
             return true;
@@ -19,14 +22,16 @@ public class UserRegistrationValidation {
         }
     }
 
+    //regex pattern for email id
     public boolean validateEmailId(String emailId) throws UserRegistrationValidationException {
-        if (Pattern.matches("[a-z][a-z A-Z 0-9]+[@][a-z]+[.][a-z]{2,5}", emailId)) {
+        if (Pattern.matches("[a-z][a-z A-Z 0-9_!#$%&'*+/=?`{|}~^.-]+[@][a-z]+[.][a-z]{2,5}+[.][a-z]{2,3}", emailId)) {
             return true;
         } else {
             throw new UserRegistrationValidationException("Entered invalid email id");
         }
     }
 
+    //regex pattern for mobile number
     public boolean validateMobileNumber(String mobileNumber) throws UserRegistrationValidationException {
         if (Pattern.matches("[+]91 [6789][0-9]{9}", mobileNumber)) {
             return true;
@@ -35,6 +40,7 @@ public class UserRegistrationValidation {
         }
     }
 
+    //regex pattern for password
     public boolean validatePassword(String password) throws UserRegistrationValidationException {
         if (Pattern.matches("(?=.*[A-Z])(?=.*\\d)(?=.*[$&+,:;=?@#|'<>.-^*()%!])[a-z A-Z 0-9 $&+,:;=?@#|'<>.-^*()%!]{8,}", password)) {
             return true;
